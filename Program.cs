9aed800fbbbd2d3c6370339a51eda17cc7781894
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-21 13:04:42 +0530

    Remove unnessary files

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using employee_mangment_backend.models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<EmployeeContext>(opt =>
+    opt.UseInMemoryDatabase("Employees"));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "EmployeeApi", Version = "v1" });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +24,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+   
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "employeeApi v1"));
 }
 
 app.UseHttpsRedirection();
