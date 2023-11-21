44927abd5d2e329f651cfdf5729f5e783441e86a
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-21 13:04:20 +0530

    Add getAllEmployee method in EmployeeController

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using employee_mangment_backend.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace employee_mangment_backend.Controllers
+{
+    [ApiController]
+    [Route("/api/employees")]
+    public class EmployeeController : Controller
+    {
+
+        private readonly EmployeeContext _employeecontext;
+        public EmployeeController(EmployeeContext employeeContext)
+        {
+            _employeecontext = employeeContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> getAllEmployees()
+        {
+            var employees =  await  _employeecontext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
