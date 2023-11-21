6112937d2747960e51de73d94b52993b627eaa90
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-21 13:12:00 +0530

    Add createEmployee Method in EmployeeController

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -22,5 +22,14 @@ namespace employee_mangment_backend.Controllers
             var employees =  await  _employeecontext.Employees.ToListAsync();
             return Ok(employees);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> createEmployee([FromBody]Employee employee)
+        {
+            employee.id = Guid.NewGuid();
+            await  _employeecontext.Employees.AddAsync(employee);
+            await _employeecontext.SaveChangesAsync();
+            return Ok(employee);
+        }
     }
 }
